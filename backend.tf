66f174a4499c41883ed7929e8f7516e5c765f4a8
Bogdan Grozoiu <50367646+bogdan-grozoiu@users.noreply.github.com>
2025-05-24 21:13:02 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,15 @@
 terraform { 
   cloud { 
-    
     organization = "cloud-positive" 
-
     workspaces { 
       name = "website-infra" 
     }
-
-  } 
+  }
+	required_providers {
+		random = {
+			 source  = "hashicorp/random"
+			 version = "3.3.2"
+			 }
+		}
+	required_version = ">= 1.1.0"
 }
-
